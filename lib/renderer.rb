35dceba0bc6a7109c12a7a7ac8f718597efc7c63
Sourav Goswami <souravgoswami@protonmail.com>
2020-05-06 13:18:30 +0530

    Modified lib/renderer.rb

--- lib/renderer.rb
+++ lib/renderer.rb
@@ -1,7 +1,7 @@
 module DevcampViewTool123456
 	class Renderer
-		def self.copyright
-			"&copy; #{Time.now.year} | <b>#{name.split.each(&:capitalize).join(' ')}</b> #{msg.split.each(&:capitalize!).join(' ')}"
+		def self.copyright(name, msg)
+			"&copy; #{Time.now.year} | <b>#{name.split.each(&:capitalize!).join(' ')}</b> #{msg.split.each(&:capitalize!).join(' ')}"
 		end
 	end
 end
